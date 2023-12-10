3bd2ef21947bed798a92b636c52bc531074fb012
Dean McNevin <deaner1456@gmail.com>
2023-12-10 15:14:29 -0600

    worked on client.js more

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -18,3 +18,51 @@ function calculateNumbers(event) {
     resultHistory.secondNumber;
     event.preventDefault();
 }
+
+axios({
+    method: "POST",
+    url: "/calculations",
+    data: currentResults
+  })
+  .then((response)=>{
+    console.log('successfully added')
+    displayResults()
+  })
+  .catch((error)=>{
+    console.log("server error", error);
+  })
+document.getElementById("calculator").reset()
+}
+
+function displayResults(){
+
+
+    // Axios GET request
+    axios({
+        method: "GET",
+        url: "/calculations",
+      })
+      .then((response)=>{
+        console.log('response from get', response.data)
+       let incArray = response.data
+       // display history
+       let latestResult =incArray[incArray.length-1]
+       let resultHistory = document.getElementById("resultHistory")
+       resultHistory.innerHTML =''
+       for(let items of incArray){
+        resultHistory.innerHTML+=  `<div>${items.numOne} ${items.operator} ${items.numTwo} = ${items.result}</div>`
+    }
+
+    // display latest result
+    let recentResult = document.getElementById('recentResult')
+    recentResult.innerHTML = `<h2>Result: ${latestResult.result}</h2>`
+   })
+   .catch((error)=>{
+     console.log("server error", error);
+   })
+}
+
+function clearInputs(event){
+    event.preventDefault()
+    document.getElementById("calculator").reset()
+  }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.static('server/public'));
