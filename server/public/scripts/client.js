cedb613bbae0872464c69083ad8744d4993586e8
Dean McNevin <deaner1456@gmail.com>
2023-12-10 13:56:52 -0600

    created two functions on client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,20 @@
 console.log('client.js is sourced!');
+
+displayCalculationResults();
+
+let resultHistory = {};
+
+function operatorEvent (event) {
+    let newOperator = event.target.textContent
+    resultHistory.operator = newOperator
+    console.log(resultHistory);
+    event.preventDefault();
+}
+
+function calculateNumbers(event) {
+    let firstNumber = document.getElementById("firstNumber").value
+    let secondNumber = document.getElementById("secondNumber").value
+    resultHistory.firstNumber;
+    resultHistory.secondNumber;
+    event.preventDefault();
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 let PORT = process.env.PORT || 5000;
 
 app.use(express.json());
